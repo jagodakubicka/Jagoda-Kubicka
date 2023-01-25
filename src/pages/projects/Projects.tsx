927eb484008fe65445f6c16affb75e4f28aa01b7
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2023-01-25 18:37:43 +0100

    framermotion removed from projects

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import Projects from '../../components/projects/Projects'
-import {motion} from "framer-motion"
+
 
 export default function Portfolio() {
   return (
-    <motion.div
-    initial={{opacity: 0}}
-    animate={{opacity: 1}}
-    exit={{opacity:0}}
-    >
-      <Projects />
-    </motion.div>
+  <>
+    <Projects />
+  </>
   )
 }
