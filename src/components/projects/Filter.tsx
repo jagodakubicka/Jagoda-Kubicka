684c982e3854ec4d7ab697834222a66abb61a93a
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2023-02-06 20:37:46 +0100

    buttons update

--- src/components/projects/Filter.tsx
+++ src/components/projects/Filter.tsx
@@ -1,8 +1,9 @@
 // @ts-nocheck
-import { useEffect } from "react";
+import { useEffect} from "react";
 import data from './data.json';
 
 function Filter({setActiveBtn, activeBtn, setFiltered, project}){
+
 
  useEffect(() => {
   if(activeBtn === 'all'){
@@ -14,7 +15,6 @@ function Filter({setActiveBtn, activeBtn, setFiltered, project}){
   setFiltered(filtered);
  }, [activeBtn]);
 
-
 return(
  <div className="project-filterCtn">
    <button 
@@ -23,15 +23,50 @@ return(
     >All
     </button>
 
-  {data.map(({tags, id}) => (
+     <button 
+    className={`project__filterBtn ${activeBtn === 'HTML' ? "project__filterBtn--active" : ""}`}
+    onClick={()=> setActiveBtn('HTML')}
+    >HTML
+    </button>
+
+     <button 
+    className={`project__filterBtn ${activeBtn === 'CSS' ? "project__filterBtn--active" : ""}`}
+    onClick={()=> setActiveBtn('CSS')}
+    >CSS
+    </button>
+
+    <button 
+    className={`project__filterBtn ${activeBtn === 'SCSS' ? "project__filterBtn--active" : ""}`}
+    onClick={()=> setActiveBtn('SCSS')}
+    >SCSS
+    </button>
+
+     <button 
+    className={`project__filterBtn ${activeBtn === 'JavaScript' ? "project__filterBtn--active" : ""}`}
+    onClick={()=> setActiveBtn('JavaScript')}
+    >JavaScript
+    </button>
+
+    <button 
+    className={`project__filterBtn ${activeBtn === 'VUE' ? "project__filterBtn--active" : ""}`}
+    onClick={()=> setActiveBtn('VUE')}
+    >VUE
+    </button>
+
+  {/* {data.map(({tags, id}) => (
     <div key={id}>
-    {tags.map((btn => <button 
+
+    {tags.map((btn => 
+    <button 
      className={`project__filterBtn ${activeBtn === btn ? "project__filterBtn--active" : ""}`} 
     key={btn}
     onClick={()=> setActiveBtn(btn)}
-    >{btn}</button>))}
+    >{btn}
+    </button>
+    ))}
     </div>
-  ))}
+  ))} */}
+
  </div>
 )
 }
